ae5afca699ae872d4e850276b027cfab0e5cb037
Mahipalsinh <mahipalsinh.dabhi@streebo.com>
2020-05-03 17:42:02 +0530

    new project

---
+++ precache-manifest.b0fff36e76fe3b1a01b40d454f2f3cc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdf8a8e6c5109fa346e2fea68190ced",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31cf7842bb8b2b8325e9",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "cc1525197af5f4dc33e5",
+    "url": "/static/css/main.f193d2ce.chunk.css"
+  },
+  {
+    "revision": "31cf7842bb8b2b8325e9",
+    "url": "/static/js/2.01235e26.chunk.js"
+  },
+  {
+    "revision": "213bca7ce55773509172dbee394af2a1",
+    "url": "/static/js/2.01235e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1525197af5f4dc33e5",
+    "url": "/static/js/main.4763a005.chunk.js"
+  },
+  {
+    "revision": "01487e499810510433fa",
+    "url": "/static/js/runtime-main.ecae8343.js"
+  }
+]);
